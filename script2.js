a627aaa369bc1a935bcb34d03afa2cf5cac0314d
Harshit singhal <harshitsinghal252@gmail.com>
2022-01-16 22:30:52 +0530

    new

--- script2.js
+++ script2.js
@@ -35,44 +35,75 @@ const getGas = () => {
       const oneGascost = fee * gweiToEth;
       LowGascost = 21000 * oneGascost;
       LowGascost = LowGascost.toFixed(2);
+      const lowgaswei= lowgas/1000000000;
 
       const mediumgas = data.result.ProposeGasPrice;
       const medgweiToEth = mediumgas / 1000000000;
       const medoneGascost = fee * medgweiToEth;
       MediumGascost = 21000 * medoneGascost;
       MediumGascost = MediumGascost.toFixed(2);
+      const  mediumgaswei= mediumgas/1000000000;
 
       const highgas = data.result.FastGasPrice;
       const fastgweiToEth = highgas / 1000000000;
       const fastoneGascost = fee * fastgweiToEth;
       FastGasCost = 21000 * fastoneGascost;
       FastGasCost = FastGasCost.toFixed(2);
+      const highgaswei= highgas/1000000000;
 
       LowGas.innerHTML= lowgas+"Gwei";
       MedGas.innerHTML= mediumgas+"Gwei";
       FastGas.innerHTML= highgas+ "Gwei";
+
+      // ConfirmationTime(lowgaswei,mediumgaswei,highgaswei);
     });
 };
 
-// function Pricefunc() {
+// function ConfirmationTime(LowGasPrice, MedGasPrice, FastGasPrice) {
 //   fetch(
-//     "https://api.etherscan.io/api?module=stats&action=ethprice&apikey=NS4S9UP9T8TDMPVI9H9RWH572AKWQPI4UN"
+//     "https://api.etherscan.io/api?module=gastracker&action=gasestimate&gasprice=" +
+//       LowGasPrice +
+//       "&apikey=NS4S9UP9T8TDMPVI9H9RWH572AKWQPI4UN"
 //   )
 //     .then((data) => data.json())
-//     .then((data) => {
-//       price = data.result.ethusd;
+//     .then((timing) => {
+//       const Lowtime = timing.result;
+//       const LowTime = document.getElementById("Lowtime");
+//       LowTime.innerHTML = Lowtime;
+//     });
+//   fetch(
+//     "https://api.etherscan.io/api?module=gastracker&action=gasestimate&gasprice=" +
+//       MedGasPrice +
+//       "&apikey=NS4S9UP9T8TDMPVI9H9RWH572AKWQPI4UN"
+//   )
+//     .then((data) => data.json())
+//     .then((timing) => {
+//       const Medtime = timing.result;
+//       const MedTime = document.getElementById("MedTime");
+//       MedTime.innerHTML = Medtime;
+//     });
+
+//   fetch(
+//     "https://api.etherscan.io/api?module=gastracker&action=gasestimate&gasprice=" +
+//       FastGasPrice +
+//       "&apikey=NS4S9UP9T8TDMPVI9H9RWH572AKWQPI4UN"
+//   )
+//     .then((data) => data.json())
+//     .then((timing) => {
+//       const fasttime = timing.result;
+//       const FastTime = document.getElementById("FastTime");
+//       FastTime.innerHTML = fasttime;
 //     });
 // }
 
 setInterval(() => {
-  // Pricefunc();
   getFee();
   if (fee !== null) {
     getGas();
   }
   console.log(Main);
   console.log(Loader);
-  if (fee && LowGascost && MediumGascost && FastGasCost ) {
+  if (fee && LowGascost && MediumGascost && FastGasCost  ) {
     Ethprice.innerHTML = "$" + fee;
     LowgasDollar.innerHTML = "$" + LowGascost;
     MediumgasDollar.innerHTML = "$" + MediumGascost;
